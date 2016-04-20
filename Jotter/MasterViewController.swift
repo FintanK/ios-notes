049f605e2648b4871fa7ae619440f44c3bb59765
fintan <fintan@bravenew.world>
2016-04-20 12:12:33 -0300

    Initial commit

--- Jotter/MasterViewController.swift
+++ Jotter/MasterViewController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
+var objects:[String] = [String]()
+var currentIndex:Int = 0
+var masterView:MasterViewController?
+var detailViewController:DetailViewController?
+
+// Key for data in persistent storage
+let kNotes:String = "notes"
+let BLANK_NOTE:String = "(New Note)"
+
+
+
 class MasterViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        masterView = self
+        load()
+        
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
     }
 
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        
+        // Persist our data
+        save()
+        
         super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if objects.count == 0 {
+            insertNewObject(self)
+        }
+        
+        super.viewDidAppear(animated)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,23 +61,46 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Create a new note
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        
+        // Only create a new note outside of editing mode
+        if detailViewController?.detailDescriptionLabel.editable == false {
+            return
+        }
+        
+        // Prevent multiple blank notes
+        if objects.count == 0 || objects[0] != BLANK_NOTE {
+            objects.insert(BLANK_NOTE, atIndex: 0)
+            let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+            self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+        
+        currentIndex = 0
+        
+        // Show detail view
+        self.performSegueWithIdentifier("showDetail", sender: self)
+        
+        
     }
 
-    // MARK: - Segues
-
+    // Prepare for transition from master view to detail view (notes view to note view)
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        detailViewController?.detailDescriptionLabel.editable = true
+
+        
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
+                currentIndex = indexPath.row
             }
+            
+            let object = objects[currentIndex]
+            detailViewController?.detailItem = object
+            detailViewController?.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+            detailViewController?.navigationItem.leftItemsSupplementBackButton = true
+            
         }
     }
 
@@ -70,8 +117,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
@@ -80,12 +127,56 @@ class MasterViewController: UITableViewController {
         return true
     }
 
+    // Handling deletes
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    // Don't save deletions if editing deletions - only delete when done
+
+    override func setEditing(editing: Bool, animated: Bool) {
+        
+        super.setEditing(editing, animated: animated)
+        
+        if editing {
+            detailViewController?.detailDescriptionLabel.editable = false
+            detailViewController?.detailDescriptionLabel.text = ""
+            return
+        }
+        
+        save()
+    }
+    
+    // wWhen an item is deleted when swiping to the left
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        detailViewController?.detailDescriptionLabel.editable = false
+        detailViewController?.detailDescriptionLabel.text = ""
+        save()
+    }
+    
+
+    
+    // Saving and loading from device memory / disk
+    
+    func save() {
+        
+        NSUserDefaults.standardUserDefaults().setObject(objects, forKey: kNotes);
+        
+        // Persist to disk straight away in case the app crashes
+        NSUserDefaults.standardUserDefaults().synchronize();
+        
+    }
+    
+    func load() {
+        // Idf data exists
+        if let loadedData = NSUserDefaults.standardUserDefaults().arrayForKey(kNotes) as? [String] {
+            objects = loadedData
         }
     }
 
